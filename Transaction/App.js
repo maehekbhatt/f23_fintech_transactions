5574e8ac7c1055eb3478bc851362ba30bcfb7a35
Chenyi Wang <chenyi@utexas.edu>
2023-10-09 16:36:41 -0500

    Added front page on App.js

--- Transaction/App.js
+++ Transaction/App.js
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>UT Safe Trade</Text>
+      <Text style={styles.description}>Build 0.0</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3498db', // Background color (Blue)
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff', // Text color (White)
+    marginBottom: 20,
+  },
+  description: {
+    fontSize: 18,
+    color: '#ecf0f1', // Text color (Light Gray)
   },
 });
