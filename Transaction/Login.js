46d89ec78d9f65e084f1688bbce8024ae62c8703
Chenyi Wang <chenyi@utexas.edu>
2023-10-25 19:02:34 -0500

    Added .edu confirmation

--- Transaction/Login.js
+++ Transaction/Login.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleButtonPress = () => {
-    // Navigate to the new page when the button is pressed
-    navigation.navigate('Home');
+    if (email.trim() === '' || password.trim() === '') {
+      // Display an alert if either the email or password is blank
+      Alert.alert('Missing Information', 'Please enter both email and password.');
+    } else if (!validateEmail(email)) {
+      // Display an alert for invalid email
+      Alert.alert('Invalid Email', 'Please use a .edu email address.');
+    } else {
+      // Navigate to the new page when the input is valid
+      navigation.navigate('Home');
+    }
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /.edu$/i; // Check if the email ends with ".edu"
+    return emailRegex.test(email);
   };
 
   return (
@@ -13,22 +29,25 @@ export default function LoginScreen({ navigation }) {
       <Text style={styles.description}>Log In</Text>
       <TextInput
         style={styles.input}
-        placeholder="Email"
+        placeholder="Log in with your .edu email"
         keyboardType="email-address"
         autoCapitalize="none"
         textContentType="emailAddress"
         spellCheck={false}
         width={300}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
       />
       <TextInput
         style={styles.input}
         placeholder="Password"
         secureTextEntry={true}
         width={300}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
       />
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={[styles.button]}
-          onPress={handleButtonPress}>
+        <TouchableOpacity style={[styles.button]} onPress={handleButtonPress}>
           <Text style={styles.buttonText}>Log In</Text>
         </TouchableOpacity>
       </View>
@@ -68,7 +87,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   button: {
-    backgroundColor: '#e74c3c',
+    backgroundColor: '#355c7d',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
